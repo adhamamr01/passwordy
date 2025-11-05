5c312e37848649668fae5ab9e5e025adfc380590
adhamamr01 <adhamamr01.aa@gmail.com>
2025-11-05 23:06:38 +0200

    Initial commit

---
+++ src/main/java/com/passwordy/passwordy/PasswordyApplication.java
@@ -0,0 +1,13 @@
+package com.passwordy.passwordy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PasswordyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PasswordyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/passwordy/passwordy/PasswordyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passwordy.passwordy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PasswordyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
